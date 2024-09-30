343422f02d5e1656a7e75c6d07ca8f1dba2b3a9d
Ben McLean <benmclean007@gmail.com>
2024-09-30 16:31:51 +1000

    Support directly providing JWT

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ from lemmypostbot import Config, LemmyPostBot
 
 if __name__ == "__main__":
     lemmy = LemmyHttp(os.environ["LEMMY_INSTANCE"])
-    lemmy.login(os.environ["LEMMY_USERNAME"], os.environ["LEMMY_PASSWORD"])
+
+    if "LEMMY_USERNAME" in os.environ and "LEMMY_PASSWORD" in os.environ:
+        lemmy.login(os.environ["LEMMY_USERNAME"], os.environ["LEMMY_PASSWORD"])
+    elif "LEMMY_JWT" in os.environ:
+        lemmy.set_jwt(os.environ["LEMMY_JWT"])
+    else:
+        raise Exception("Please provide either LEMMY_USERNAME and LEMMY_PASSWORD, or LEMMY_JWT")
+
+
     with open("config.yml", "r") as ymlfile:
         content = ymlfile.read()
     config = Config.from_yaml(content)
